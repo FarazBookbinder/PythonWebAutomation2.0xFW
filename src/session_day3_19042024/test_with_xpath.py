fd78d84edc33276ce2774b3bdee0ea6a58cc7ec5
FarazBookbinder <muaaviya26@gmail.com>
2024-04-24 00:50:52 +0530

    My new commit

--- src/session_day3_19042024/test_with_xpath.py
+++ src/session_day3_19042024/test_with_xpath.py
@@ -9,17 +9,16 @@ from allure_commons.types import AttachmentType
 # <a id="btn-make-appointment" href="./profile.php#login" class="btn btn-dark btn-lg">Make Appointment</a>
 @pytest.mark.smoke
 @allure.title("This is the Mini project")
-def test_navigate_by_link_text():
+def test_navigate_by_xpath():
     driver = webdriver.Chrome()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
     driver.maximize_window()
     allure.attach(driver.get_screenshot_as_png(), name="Screenshot2", attachment_type=AttachmentType.PNG)
-    list_appt_btn = driver.find_elements(By.TAG_NAME, "a")
-    make_apt_btn = list_appt_btn[5]
-    make_apt_btn.click()
-    time.sleep(10)
+    appointment_btn = driver.find_element(By.XPATH, "//a[@id='btn-make-appointment']")
+    appointment_btn.click()
+    time.sleep(5)
     assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login"
     allure.attach(driver.get_screenshot_as_png(), name="Screenshot1", attachment_type=AttachmentType.PNG)
-    time.sleep(15)
+    time.sleep(5)
     print(driver.current_url)
     driver.quit()
