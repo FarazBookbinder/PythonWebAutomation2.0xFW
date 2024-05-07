02103f7561968e6eaa5a74c6b64bc22ea5869870
FarazBookbinder <muaaviya26@gmail.com>
2024-05-08 01:42:25 +0530

    My new commit

---
+++ src/session_day09_03052024/test_JS_alerts.py
@@ -0,0 +1,25 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_js_alerts():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+    driver.maximize_window()
+    time.sleep(5)
+    button = driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']")
+    button.click()
+
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.alert_is_present())
+    alert = driver.switch_to.alert
+    alert.send_keys("Faraz")
+    alert.accept()
+    results = driver.find_element(By.XPATH," //p[@id='result']")
+    print(results.text)
+    driver.quit()
